a74c4429062b569337ec4472bd01968c2f8de22e
gitcupcake <nurosman.isik@stu.fsm.edu.tr>
2025-04-24 18:10:54 +0300

    selam

--- src/dataproject/FrmMenu.java
+++ src/dataproject/FrmMenu.java
@@ -70,7 +70,8 @@ public class FrmMenu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       
+        new FrmGame().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
